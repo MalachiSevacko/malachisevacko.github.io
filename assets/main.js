d598ab8ed1dce5e5944f6e4516dbddec63b2ef13
MalachiSevacko <msevack1@kent.edu>
2025-10-18 10:36:54 -0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,11 @@
+// Auto-mark the current page in the navbar
+(function () {
+  const here = location.pathname.split('/').pop() || 'index.html';
+  document.querySelectorAll('nav a').forEach(a => {
+    const target = a.getAttribute('href');
+    if ((here === '' && target === 'index.html') || here === target) {
+      a.setAttribute('aria-current', 'page');
+    }
+  });
+})();
 
